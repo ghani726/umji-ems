d4fb47edc230e255dd0757b380e2d099ef5dce2d
Muhammad Ghani Ul Hassan <ghani726@outlook.com>
2026-05-27 03:13:26 +0500

    Main Homepage done, also responsive footer made. Now make the app

--- src/Components/Web/Footer.jsx
+++ src/Components/Web/Footer.jsx
@@ -1,28 +1,56 @@
+import { Link } from "react-router-dom"
+
+import logo from "../../../public/logo.png"
+import { FaGithub } from "react-icons/fa"
 
 const Footer = () => {
   return (
-    <footer className="bg-white w-full p-12 px-16">
-        <section className="flex items-center justify-between">
-            <div className="flex flex-col">
-                <div className="flex">
-                    <div></div>
-                    <h4>UMJI EMS</h4>
+    <footer className="bg-white w-full p-12 px-4 sm:px-12 lg:px-32 xl:px-64">
+        <section className="flex flex-col sm:flex-row items-center justify-between gap-8 lg:gap-24 lg:pr-16 w-full">
+            <div className="flex flex-col items-center sm:items-start gap-4">
+                <div className="flex items-center gap-2">
+                    <div className="shadow-[0_0_10px_rgba(0,0,0,0.3)] rounded-xl overflow-hidden">
+                        <img src={logo} className="h-12 w-12" alt="UMJI EMS Logo" />
+                    </div>
+                    <h4 className="text-xl font-bold ">UMJI EMS</h4>
                 </div>
-                <p className="text-secondary-700">The all-in-one platform for modern HR teams.</p>
+                <p className="text-secondary-700 w-full items-center sm:items-start">The all-in-one platform for modern HR teams.</p>
             </div>
-            <div>
-                <div className="flex flex-col">
-                    <h4>Support</h4>
-                    <div>
-                        <p className="text-secondary-700">Pricing</p>
-                        <p className="text-secondary-700">Guides</p>
-                        <p className="text-secondary-700">Documentaion</p>
+            <div className="flex flex-col sm:flex-row items-center sm:justify-between sm:items-start w-full gap-4">
+                <div className="flex flex-col items-center sm:items-start">
+                    <h4 className="text-lg font-bold ">Support</h4>
+                    <div className="flex flex-col items-center sm:items-start" >
+                        <Link className="cursor-pointer text-secondary-700">Pricing</Link>
+                        <Link className="cursor-pointer text-secondary-700">Guides</Link>
+                        <Link className="cursor-pointer text-secondary-700">Documentaion</Link>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center sm:items-start">
+                    <h4 className="text-lg font-bold ">Company</h4>
+                    <div className="flex flex-col items-center sm:items-start" >
+                        <Link className="cursor-pointer text-secondary-700">About</Link>
+                        <Link className="cursor-pointer text-secondary-700">Vision</Link>
+                        <Link className="cursor-pointer text-secondary-700">Testimonials</Link>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center sm:items-start">
+                    <h4 className="text-lg font-bold ">Legal</h4>
+                    <div className="flex flex-col items-center sm:items-start">
+                        <Link className="cursor-pointer text-secondary-700">Privacy</Link>
+                        <Link className="cursor-pointer text-secondary-700">Terms</Link>
+                        {/* <Link className="cursor-pointer text-secondary-700"></Link> */}
                     </div>
                 </div>
             </div>
         </section>
-        <section>
-
+        <hr className="my-8 bg-secondary-700 text-secondary-700 border-none h-0.5 rounded-4xl"/>
+        <section className="flex justify-between items-center">
+            <div>© 2026 UMJI EMS. All rights reserved.</div>
+            <div>
+                <FaGithub className="text-2xl cursor-pointer" onClick={()=>{
+                    window.open("https://github.com/ghani726/umji-ems")
+                }} />
+            </div>
         </section>
     </footer>
   )
--- src/pages/Website/Home.jsx
+++ src/pages/Website/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <section className="flex flex-col justify-center items-center p-8 lg:p-24 md:p-16 lg:px-32 md:px-24 sm:px-16 gap-6 border-b-secondary-300 border-b">
         <div className="flex flex-col justify-center items-center  sm:items-start md:items-center lg:items-center">
           <h2 className="lg:text-6xl md:text-5xl sm:text-4xl text-3xl font-bold">Manage Your Team</h2>
-          <h2 className="lg:text-6xl md:text-5xl sm:text-4xl text-3xl font-bold text-primary-700">Efficiently & Seamlessly</h2>
+          <h2 className="lg:text-6xl md:text-5xl sm:text-4xl text-3xl font-bold text-primary-700 text-center">Efficiently & Seamlessly</h2>
         </div>
         <p className="text-secondary-700  max-w-200 text-left xs:text-center">The all-in-one platform to streamline your processes, from payroll and attendance to leave management and announcements. Empower your workforce and simplify your administration.</p>
         <div className="flex justify-between sm:justify-start items-center gap-4">
@@ -17,7 +17,7 @@ const Home = () => {
       </section>
       <section className="flex flex-col justify-center items-center p-8 md:p-12 lg:px-32 md:px-24 sm:px-16 gap-4">
         <h4 className="text-lg text-primary-700 font-semibold">Our Features</h4>
-        <h3 className="lg:text-4xl md:text-5xl sm:text-4xl text-3xl font-bold">A better way to manage your workforce</h3>
+        <h3 className="lg:text-4xl md:text-5xl sm:text-4xl text-3xl text-center font-bold">A better way to manage your workforce</h3>
         <p className="text-secondary-700  max-w-125 text-left xs:text-center">Everything you need to run your HR operations smoothly and efficiently, all in one place.</p>
         <section className="grid grid-cols-[repeat(auto-fit,minmax(max(300px,calc(50%-1rem)),1fr))] gap-4">
           <div className="feature flex justify-between items-center bg-white p-4 rounded-2xl gap-4">
